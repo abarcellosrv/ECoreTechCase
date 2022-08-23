aaeead9b0cde461e49900531f0b38eff79a6c7a1
abarcellos <amandabrvalle@gmail.com>
2022-08-23 12:25:04 -0300

    created text.js file

---
+++ text.js
@@ -0,0 +1,31 @@
+const person = require('./Person');
+
+function greeting() {
+    console.log("==============================================================");
+    console.log("Welcome to the Human Repository!");
+    console.log("Here you will be able to add humans to our records and see ordered lists of all humans ever entered into it.");
+}
+
+function menuItems() {
+    console.log("Enter the number corresponding to desired option:");
+    console.log("[1] Add human to Repository");
+    console.log("[2] List humans by age");
+    console.log("[3] List humans alphabetically");
+    console.log("[4] List humans in chosen age group");
+    console.log("[5] Exit Repository");
+}
+
+function ageGroupOptions() {
+    console.log("Enter number corresponding to desired age group:");
+    console.log("[1] Child");
+    console.log("[2] Teenager");
+    console.log("[3] Adult");
+    console.log("[4] Elderly");
+}
+
+function addedHumanConfirmation(person) {
+    console.log("You wish to add the following person: ");
+    person.toString();
+    console.log("Is this correct?");
+    console.log("If so, enter 1 to confirm");
+}
